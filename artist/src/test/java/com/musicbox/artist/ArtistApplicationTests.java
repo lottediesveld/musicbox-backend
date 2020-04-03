4763370e01c918c9bc200c4b2291fb321be5e0aa
Lotte Diesveld <l.diesveld@student.fontys.nl>
2020-04-03 11:51:27 +0200

    Setup project

---
+++ artist/src/test/java/com/musicbox/artist/ArtistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicbox.artist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
