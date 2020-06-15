a4b9a0eca991c687778ee38bf786a291ab8a4dff
lottediesveld <l.diesveld@student.fontys.nl>
2020-06-15 16:49:50 +0200

    removed loadbalance annotation

--- auth-service/src/main/java/com/musicbox/auth/AuthApplication.java
+++ auth-service/src/main/java/com/musicbox/auth/AuthApplication.java
@@ -2,7 +2,6 @@ package com.musicbox.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +19,6 @@ public class AuthApplication {
 
         // Create a bean for restTemplate to call services
         @Bean
-        @LoadBalanced        // Load balance between service instances running at different ports.
         public RestTemplate restTemplate() {
             return new RestTemplate();
         }
